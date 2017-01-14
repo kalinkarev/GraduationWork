8eec05175773ec9f6e6b95ea589632db63d26caa
HPProbook4540s\Kalin <karev.kalin@gmail.com>
2017-01-14 20:16:50 +0200

    Adding the UI of the New Event screen. Almost done

---
+++ GraduationWork/app/src/main/java/com/example/kalin/graduationwork/NewEvent.java
@@ -0,0 +1,17 @@
+package com.example.kalin.graduationwork;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class NewEvent extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+    }
+
+}
