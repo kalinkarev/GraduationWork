52c9754386cbfd53883810e63a800834a633fa0e
HPProbook4540s\Kalin <karev.kalin@gmail.com>
2017-01-14 06:55:02 +0200

    Adding the hardcoded activities

---
+++ GraduationWork/app/src/main/java/com/example/kalin/graduationwork/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.kalin.graduationwork;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(MainActivity.this, NewEvent.class);
+                startActivity(i);
+            }
+        });
+
+        String[] activities = {"Tennis", "Fitness", "Free"};
+        ListAdapter kalinsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, activities);
+        ListView kalinsListView = (ListView) findViewById(R.id.kalinsListView);
+        kalinsListView.setAdapter(kalinsAdapter);
+
+        kalinsListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String activity = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(MainActivity.this, activity, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
