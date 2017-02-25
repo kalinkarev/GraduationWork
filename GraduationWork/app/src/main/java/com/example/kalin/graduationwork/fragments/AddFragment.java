2109239fb52ad67382fd209635c7081c47a18b71
HPProbook4540s\Kalin <karev.kalin@gmail.com>
2017-02-25 22:24:02 +0200

    Adding the timepicker to the code. It is working

--- GraduationWork/app/src/main/java/com/example/kalin/graduationwork/fragments/AddFragment.java
+++ GraduationWork/app/src/main/java/com/example/kalin/graduationwork/fragments/AddFragment.java
@@ -1,11 +1,15 @@
 package com.example.kalin.graduationwork.fragments;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.kalin.graduationwork.R;
 
@@ -24,8 +28,10 @@ public class AddFragment extends BaseFragment {
 
     int mYear, mMonth, mDay, mDayWeek, mHour, mMinute;
 
-    TextView tvForDate;
-//    TextView tvForFinishDate;
+    TextView tvForStartDate;
+    TextView tvForFinishDate;
+    TextView tvForStartTime;
+    TextView tvForFinishTime;
 
     @Override
     protected int getLayoutId() {
@@ -37,122 +43,82 @@ public class AddFragment extends BaseFragment {
 
         String formatCurrentDate = DateFormat.getDateInstance(DateFormat.FULL).format(new Date());
 
-        tvForDate = (TextView) mainView.findViewById(R.id.textViewForStartDate);
+        tvForStartDate = (TextView) mainView.findViewById(R.id.textViewForStartDate);
+        tvForFinishDate = (TextView) mainView.findViewById(R.id.textViewForFinishDate);
 
-        tvForDate.setText(formatCurrentDate);
+        tvForStartDate.setText(formatCurrentDate);
+        tvForFinishDate.setText(formatCurrentDate);
 
-        tvForDate.setOnClickListener(new View.OnClickListener(){
+        tvForStartDate.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                final Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-
-                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener(){
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        calendar.set(Calendar.YEAR, year);
-                        calendar.set(Calendar.MONTH, monthOfYear);
-                        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-
-                        monthOfYear = monthOfYear + 1;
-
-                        String format = "yyyy-MM-dd";
-
-                        tvForDate.setText(dayOfMonth+"-"+monthOfYear+"-"+year);
-                    }
-                }, mYear, mMonth, mDay);
-
-                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
-                datePickerDialog.show();
+                    datePickerStartDate();
             }
         });
 
-//        tvForStartDate = (TextView) mainView.findViewById(R.id.textViewForStartDate);
-//        tvForFinishDate = (TextView) mainView.findViewById(R.id.textViewForFinishDate);
-//
-//        final TextView tvForStartTime = (TextView) mainView.findViewById(R.id.textViewForStartTime);
-//        final TextView tvForFinishTime = (TextView) mainView.findViewById(R.id.textViewForFinishTime);
-//
-//        String formatCurrentDate = DateFormat.getDateInstance(DateFormat.FULL).format(new Date());
-//
-//        tvForStartDate.setText(formatCurrentDate);
-//        tvForFinishDate.setText(formatCurrentDate);
-//
-//        tvForStartDate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
-//
-//        Switch switchForDateDuration = (Switch) mainView.findViewById(R.id.switchForDuration);
-//
-//        // set the switch to ON
-//        switchForDateDuration.setChecked(false);
-//        //attach a listener to check for changes in state
-//        switchForDateDuration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView,
-//                                         boolean isChecked) {
-//
-//                if (isChecked) {
-//                    //switchStatus.setText("Switch is currently ON");
-//                    tvForStartDate.setVisibility(View.GONE);
-//                    tvForFinishDate.setVisibility(View.GONE);
-//                } else {
-//                    //switchStatus.setText("Switch is currently OFF");
-//                    tvForStartDate.setVisibility(View.VISIBLE);
-//                    tvForFinishDate.setVisibility(View.VISIBLE);
-//                }
-//
-//            }
-//        });
-//
-//        //check the current state before we display the screen
-//        if (switchForDateDuration.isChecked()) {
-//            //switchStatus.setText("Switch is currently ON");
-//            //tvForData.setVisibility(View.GONE);
-//            tvForStartDate.setVisibility(View.GONE);
-//            tvForFinishDate.setVisibility(View.GONE);
-//        } else {
-//            //switchStatus.setText("Switch is currently OFF");
-//            //tvForData.setVisibility(View.VISIBLE);
-//            tvForStartDate.setVisibility(View.VISIBLE);
-//            tvForFinishDate.setVisibility(View.VISIBLE);
-//        }
+        tvForFinishDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                datePickerFinishDate();
+            }
+        });
+
+        String formatCurrentTime = DateFormat.getTimeInstance().format(new Date());
+
+        tvForStartTime = (TextView) mainView.findViewById(R.id.textViewForStartTime);
+        tvForFinishTime = (TextView) mainView.findViewById(R.id.textViewForFinishTime);
+
+        tvForStartTime.setText(formatCurrentTime);
+        tvForFinishTime.setText(formatCurrentTime);
+
+        tvForStartTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                timePickerStartTime();
+            }
+        });
+
+        tvForFinishTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                timePickerFinishTime();
+            }
+        });
+
+        Switch switchForDateDuration = (Switch) mainView.findViewById(R.id.switchForDuration);
+
+        // set the switch to ON
+        switchForDateDuration.setChecked(false);
+        //attach a listener to check for changes in state
+        switchForDateDuration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    //switchStatus.setText("Switch is currently ON");
+                    tvForStartTime.setVisibility(View.GONE);
+                    tvForFinishTime.setVisibility(View.GONE);
+                } else {
+                    //switchStatus.setText("Switch is currently OFF");
+                    tvForStartTime.setVisibility(View.VISIBLE);
+                    tvForFinishTime.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        //check the current state before we display the screen
+        if (switchForDateDuration.isChecked()) {
+            //switchStatus.setText("Switch is currently ON");
+            //tvForData.setVisibility(View.GONE);
+            tvForStartTime.setVisibility(View.GONE);
+            tvForFinishTime.setVisibility(View.GONE);
+        } else {
+            //switchStatus.setText("Switch is currently OFF");
+            //tvForData.setVisibility(View.VISIBLE);
+            tvForStartTime.setVisibility(View.VISIBLE);
+            tvForFinishTime.setVisibility(View.VISIBLE);
+        }
     }
-
-
-//        public void datePicker() {
-//            final Calendar c = Calendar.getInstance();
-//            c.setTimeInMillis(System.currentTimeMillis());
-//
-////        mYear = c.get(Calendar.YEAR);
-////        mMonth = c.get(Calendar.MONTH);
-////        mDay = c.get(Calendar.DAY_OF_MONTH);
-//
-//            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener(){
-//                @Override
-//                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){ //, int dayOfWeek) {
-//
-//                    c.set(Calendar.YEAR, year);
-//                    c.set(Calendar.MONTH, monthOfYear);
-//                    c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-////                c.set(Calendar.DAY_OF_WEEK, dayOfWeek);
-//
-//                    monthOfYear = monthOfYear + 1;
-//
-//                    String format = "yyyy-MM-dd";
-//
-//                    tvForStartDate.setText(dayOfMonth+","+monthOfYear+","+year);
-//
-//                }
-//            }, mYear, mMonth, mDay);
-//            datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
-//            datePickerDialog.show();
-//        }
-
 
 //        final Calendar cal = Calendar.getInstance();
 //        cal.get(Calendar.HOUR_OF_DAY);
@@ -175,25 +141,85 @@ public class AddFragment extends BaseFragment {
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
-}
 
-//    public void datePicker() {
-//        final Calendar c = Calendar.getInstance();
-//        c.setTimeInMillis(System.currentTimeMillis());
-//
-//        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener(){
-//            @Override
-//            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-//                c.set(Calendar.YEAR, year);
-//                c.set(Calendar.MONTH, monthOfYear);
-//                c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-//
-//                String format = "yyyy-MM-dd";
-//
-//                tvForDate.setText(dayOfMonth+","+monthOfYear+","+year);
-//            }
-//        }, mYear, mMonth, mDay);
-//
-//        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
-//        datePickerDialog.show();
-//    }
+    public void datePickerStartDate() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                monthOfYear = monthOfYear + 1;
+
+                String format = "yyyy-MM-dd";
+
+                tvForStartDate.setText(dayOfMonth+"-"+monthOfYear+"-"+year);
+            }
+        }, mYear, mMonth, mDay);
+
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+        datePickerDialog.show();
+
+    }
+
+    public void datePickerFinishDate() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                monthOfYear = monthOfYear + 1;
+
+                String format = "yyyy-MM-dd";
+
+                tvForFinishDate.setText(dayOfMonth+"-"+monthOfYear+"-"+year);
+            }
+        }, mYear, mMonth, mDay);
+
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+        datePickerDialog.show();
+
+    }
+
+    public void timePickerStartTime() {
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                tvForStartTime.setText(hourOfDay+":"+minute);
+
+            }
+        }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+
+    public void timePickerFinishTime() {
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                tvForFinishTime.setText(hourOfDay+":"+minute);
+
+            }
+        }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+
+}
