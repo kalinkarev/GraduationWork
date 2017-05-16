5d77a5133d4374f54d4a770b4018285f45e6e97d
HPProbook4540s\Kalin <karev.kalin@gmail.com>
2017-05-16 15:24:11 +0300

    The autocomplete for lcoation is ready

--- GraduationWork/app/src/main/java/com/example/kalin/graduationwork/fragments/AddFragment.java
+++ GraduationWork/app/src/main/java/com/example/kalin/graduationwork/fragments/AddFragment.java
@@ -6,11 +6,17 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -25,19 +31,40 @@ import com.example.kalin.graduationwork.model.ColorData;
 import com.example.kalin.graduationwork.utils.ColorUtil;
 import com.example.kalin.graduationwork.views.ColorView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-public class AddFragment extends BaseFragment implements ColorSelectedListener{
+public class AddFragment extends BaseFragment implements ColorSelectedListener, AdapterView.OnItemClickListener {
+
+    private static final String LOG_TAG = "ExampleApp";
+
+    private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+    private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+    private static final String OUT_JSON = "/json";
+
+    //------------ make your specific key ------------
+
+    private static final String API_KEY = "AIzaSyCjKffmFemdUklvZVeUZEoBnaSuOm3cGds";
 
     protected View addView;
     Toolbar toolbarAddfragment;
     Context context;
 
     EditText txtTitle;
-    EditText txtLocation;
+    AutoCompleteTextView txtLocation;
     EditText txtPrice;
     EditText txtNotification;
 
@@ -70,7 +97,7 @@ public class AddFragment extends BaseFragment implements ColorSelectedListener{
     protected void onCreateView() {
 
         txtTitle = (EditText) mainView.findViewById(R.id.editTextTitle);
-        txtLocation = (EditText) mainView.findViewById(R.id.editTextLocation);
+        txtLocation = (AutoCompleteTextView) mainView.findViewById(R.id.editTextLocation);
         txtPrice = (EditText) mainView.findViewById(R.id.editTextPrice);
         tvForStartDate = (TextView) mainView.findViewById(R.id.textViewForStartDate);
         tvForFinishDate = (TextView) mainView.findViewById(R.id.textViewForFinishDate);
@@ -182,6 +209,9 @@ public class AddFragment extends BaseFragment implements ColorSelectedListener{
 //        Editable eventTitle = txtTitle.getText();
 //        Editable location = txtLocation.getText();
 //        Editable price = txtPrice.getText();
+
+        txtLocation.setAdapter(new GooglePlacesAutocompleteAdapter(getMainActivity(), R.layout.list_item_location));
+//        txtLocation.setOnClickListener((View.OnClickListener) getActivity());
 
     }
 
@@ -305,6 +335,113 @@ public class AddFragment extends BaseFragment implements ColorSelectedListener{
         Toast.makeText(getActivity(), "You have selected " + data.getName() + " color", Toast.LENGTH_SHORT).show();
         currentColor = data;
         selectedColor();
+    }
+
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        String str = (String) adapterView.getItemAtPosition(position);
+        Toast.makeText(getMainActivity(), str, Toast.LENGTH_SHORT).show();
+    }
+
+    public static ArrayList<String> autocomplete(String input) {
+        ArrayList<String> resultList = null;
+
+        HttpURLConnection conn = null;
+        StringBuilder jsonResults = new StringBuilder();
+        try {
+            StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+            sb.append("?key=" + API_KEY);
+            sb.append("&components=country:bg");
+            sb.append("&input=" + URLEncoder.encode(input, "utf8"));
+
+            URL url = new URL(sb.toString());
+
+            System.out.println("URL: "+url);
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            // Load the results into a StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                jsonResults.append(buff, 0, read);
+            }
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error processing Places API URL", e);
+            return resultList;
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error connecting to Places API", e);
+            return resultList;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        try {
+
+            // Create a JSON object hierarchy from the results
+            JSONObject jsonObj = new JSONObject(jsonResults.toString());
+            JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
+
+            // Extract the Place descriptions from the results
+            resultList = new ArrayList<String>(predsJsonArray.length());
+            for (int i = 0; i < predsJsonArray.length(); i++) {
+                System.out.println(predsJsonArray.getJSONObject(i).getString("description"));
+                System.out.println("============================================================");
+                resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Cannot process JSON results", e);
+        }
+
+        return resultList;
+    }
+
+    class GooglePlacesAutocompleteAdapter extends ArrayAdapter<String> implements Filterable {
+        private ArrayList<String> resultList;
+
+        public GooglePlacesAutocompleteAdapter(Context context, int textViewResourceId) {
+            super(context, textViewResourceId);
+        }
+
+        @Override
+        public int getCount() {
+            return resultList.size();
+        }
+
+        @Override
+        public String getItem(int index) {
+            return resultList.get(index);
+        }
+
+        @Override
+        public Filter getFilter() {
+            Filter filter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults filterResults = new FilterResults();
+                    if (constraint != null) {
+                        // Retrieve the autocomplete results.
+                        resultList = autocomplete(constraint.toString());
+
+                        // Assign the data to the FilterResults
+                        filterResults.values = resultList;
+                        filterResults.count = resultList.size();
+                    }
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    if (results != null && results.count > 0) {
+                        notifyDataSetChanged();
+                    } else {
+                        notifyDataSetInvalidated();
+                    }
+                }
+            };
+            return filter;
+        }
     }
 
 }
