8ad42bbc05c5764f42126914809ff27659ea75a6
HPProbook4540s\Kalin <karev.kalin@gmail.com>
2017-05-27 09:36:42 +0300

    The adding into the database is ready

--- GraduationWork/app/src/main/java/com/example/kalin/graduationwork/fragments/HomeFragment.java
+++ GraduationWork/app/src/main/java/com/example/kalin/graduationwork/fragments/HomeFragment.java
@@ -12,10 +12,7 @@ import android.widget.Toast;
 import com.example.kalin.graduationwork.R;
 import com.example.kalin.graduationwork.adapter.EventsAdapter;
 import com.example.kalin.graduationwork.dao.DBManager;
-import com.example.kalin.graduationwork.model.ColorData;
-import com.example.kalin.graduationwork.model.Duration;
 import com.example.kalin.graduationwork.model.Event;
-import com.example.kalin.graduationwork.model.Location;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,117 +41,9 @@ public class HomeFragment extends BaseFragment {
 
         List<Event> events = new ArrayList<>();
 
-//        events = dbmanager.getAllEvents();
-
-        ColorData color = new ColorData();
-
-        Duration duration = new Duration();
-        duration.setStart(15);
-        duration.setFinish(16);
-        duration.setAllday(false);
-
-        Location location = new Location();
-        location.setName("Birthday");
-        location.setLongitute("100.00");
-        location.setLatitude("50.00");
-
-        Event event = new Event();
-        event.setName("Sleep");
-        event.setColor(color);
-        event.setDuration(duration);
-        event.setLocation(location);
-
-        DBManager.getInstance(getActivity()).addEvent(event, false);
-        DBManager.getInstance(getActivity()).getAllEvents();
-
-        events.add(event);
+        events = DBManager.getInstance(getActivity()).getAllEvents();
         adapter.addItems(events);
 
-
-
-
-
-//        ColorData color = new ColorData();
-////        color.setName("blue");
-//
-//        Duration duration = new Duration();
-//        duration.setStart(15);
-//        duration.setFinish(16);
-//////        duration.setRepeat(false);
-//        duration.setAllday(false);
-//
-//        Location location = new Location();
-//        location.setName("Birthday");
-//        location.setLongitute("100.00");
-//        location.setLatitude("50.00");
-//
-//        Event event = new Event();
-//        event.setName("Sleep");
-//        event.setColor(color);
-////        event.setColor(color);
-////        event.setNote("This task is for today!");
-////        event.setNotification(false);
-////        event.setPrice(50);
-//        event.setDuration(duration);
-//        event.setLocation(location);
-//
-//        DBManager.getInstance(getActivity()).addEvent(event, false);
-//        DBManager.getInstance(getActivity()).getAllEvents();
-//
-//        events.add(event);
-//        adapter.addItems(events);
-
-//        Event event = new Event();
-//        event.setName("Free");
-//        event.getId();
-//        event.getName();
-//        event.getColor();
-//        event.getNote();
-//        event.getNotification();
-//        event.getPrice();
-//        event.getDuration();
-//        event.getLocation();
-//
-//        DBManager.getInstance(getActivity()).addEvent(event, false);
-//
-//        DBManager.getInstance(getActivity()).getAllEvents();
-
-
-//        ArrayList<Event> values = new ArrayList<>();
-//
-//
-//        List<Event> events = dbmanager.getAllEvents();
-
-
-        // fill the recyclerView
-
-//        Event event1 = new Event();
-//        event1.setName("Tenis");
-//        ArrayList<Event> events = new ArrayList<>();
-//        events.add(event1);
-//        adapter.addItems(events);
-
-//        ArrayList<Event> events = new ArrayList<>();
-//        Event event1 = new Event();
-//        event1.setName("Free");
-//        Event event2 = new Event();
-//        event2.setName("Lunch");
-//        Event event3 = new Event();
-//        event3.setName("Dinner");
-//        Event event4 = new Event();
-//        event4.setName("Free");
-//        Event event5 = new Event();
-//        event5.setName("Working");
-//        Event event6 = new Event();
-//        event6.setName("Sleeping");
-//        events.add(event1);
-//        events.add(event2);
-//        events.add(event3);
-//        events.add(event4);
-//        events.add(event5);
-//        events.add(event6);
-//        adapter.addItems(events);
-//
         if (events.size() == 0) {
             list.setVisibility(View.GONE);
             layout.setVisibility(View.VISIBLE);
@@ -201,7 +90,6 @@ public class HomeFragment extends BaseFragment {
         imageViewSeeStatistics.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-//                Toast.makeText(getMainActivity(), "You have clicked the statistics button", Toast.LENGTH_SHORT).show();
 
                 getMainActivity().showFragmentAndAddToBackstack(new StatisticFragment());
 
